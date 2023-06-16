de1298d967813625c6f83b983def26d238fee513
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-06-15 22:22:05 -0500

    [add][debug] corrgiendo errores

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import nltk
+
+nltk.download('wordnet')
 
 class SentimentModel:
     def __init__(self):
@@ -14,6 +21,21 @@ class SentimentModel:
         self.is_trained = False
         self.setup_ui()
 
+    @staticmethod
+    def plot_confusion_matrix(y_true, y_pred, classes, cmap=plt.cm.Blues):
+        cm = confusion_matrix(y_true, y_pred)
+        fig, ax = plt.subplots()
+        im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+        ax.figure.colorbar(im, ax=ax)
+        ax.set(xticks=np.arange(cm.shape[1]),
+            yticks=np.arange(cm.shape[0]),
+            xticklabels=classes, yticklabels=classes,
+            title='Confusion Matrix',
+            ylabel='True label',
+            xlabel='Predicted label')
+
+        plt.show()
+
     def train(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
         X_train_dtm = self.vectorizer.fit_transform(X_train)
@@ -21,13 +43,18 @@ class SentimentModel:
         self.model.fit(X_train_dtm, y_train)
         y_pred_class = self.model.predict(X_test_dtm)
         self.is_trained = True
-        return metrics.accuracy_score(y_test, y_pred_class)
+        return X_test_dtm, y_test, y_pred_class, metrics.accuracy_score(y_test, y_pred_class)
 
     def predict(self, X):
         if not self.is_trained:
             raise Exception("Model is not trained yet!")
         X_dtm = self.vectorizer.transform(X)
         return self.model.predict(X_dtm)
+
+    def show_stats(self, X_test, y_test):
+        disp = self.plot_confusion_matrix(self.model, X_test, y_test, cmap=plt.cm.Blues)
+        disp.ax_.set_title("Confusion Matrix")
+        plt.show()
 
     def load_file(self):
         filename = filedialog.askopenfilename(filetypes=[('CSV Files', '*.csv'), ('Excel Files', '*.xlsx'), ('JSON Files', '*.json')])
@@ -42,8 +69,12 @@ class SentimentModel:
                 else:
                     messagebox.showerror("File Type Error", "Unsupported file type!")
                     return
-                accuracy = self.train(df['text'], df['sentiment'])
+
+                lemmatizer = WordNetLemmatizer()
+                df['text'] = df['text'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in x.split()]))
+                X_test, y_test, y_pred_class, accuracy = self.train(df['text'], df['sentiment'])
                 messagebox.showinfo("Model Training", f"Model trained with accuracy: {accuracy}")
+                self.show_stats(X_test, y_test)
             except Exception as e:
                 messagebox.showerror("Error loading file", str(e))
 
